8ab8be2d56e55e465696b681ea2a9372d34e9923
Dennis Dantas de Souza 113110024 <dennisds@lcc3-055.lcc.ufcg.edu.br>
2017-11-14 10:51:14 -0300

    Fix error url
    Fix error on script on post.

--- docs/scripts/boqueirao1.js
+++ docs/scripts/boqueirao1.js
@@ -1,29 +1,4 @@
----
-title: "O volume de Boqueirão"
-date: 2017-11-14T00:56:51-02:00
-draft: false
----
-# O Açude Epitácio Pessoa
-
-Em torno de Campina Grande (PB - Brazil), minha cidade de origem, há diversas cidades recebendo água de um mesmo ponto: o açude Epitácio Pessoa. Por isso, o açude de Boqueirão, como é conhecido, é bem importante na região.
-
-Nos últimos anos, a Paraíba esteve em um período longo de estiagem, particularmente maior ao redor do açude Epitácio Pessoa que teve seu nível reduzido a níveis alarmantes. Vários estudos foram feitos em busca de uma solução para o problema da água. Um deles foi a transposição do rio São Francisco, cujas obras permitiram abastecer o açude de Boqueirão em 2017.
-
-Neste post, iremos discutir dados sobre o volume do açude nas últimas duas décadas.
-
-## Volume baixo
-
-O gráfico abaixo mostra o volume percentual do açude ao longo das duas últimas décadas.
-
-<!-- markdownlint-disable MD033 -->
-<div id="vis" width=900></div>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/vega/3.0.7/vega.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/vega-lite/2.0.1/vega-lite.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/vega-embed/3.0.0-rc7/vega-embed.js"></script>
-
-<script>
-    const spec = {
+const spec = {
         "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
         "data": {
             "url": "https://api.insa.gov.br/reservatorios/12172/monitoramento",
@@ -113,6 +88,3 @@ O gráfico abaixo mostra o volume percentual do açude ao longo das duas última
         }]
     };
     vegaEmbed('#vis', spec).catch(console.warn);
-</script>
-
-<!-- markdownlint-enable MD033 -->
